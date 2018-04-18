beac2812a5277001e87a4aee0cb841e949d71692
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-04-18 11:16:43 -0300

    03) A ligação entre as ações do usuário e o modelo - 02) Associando métodos do controller às ações do usuário

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,6 +2,14 @@ class NegociacaoController{
 
     adiciona(event){
         event.preventDefault();
-        alert("Chamei ação no controller");
+       // alert("Chamei ação no controller");
+
+       //coloca a função dentro da variável com associação com o document
+       let $ = document.querySelector.bind(document);
+       let inputQuantidade = $('#quantidade');
+       let inputData = $('#data');
+       let inputValor = $('#valor');
+
+       console.log(inputData.value);
     }
 }
