8d0c68cd55e8be73450b0bf4935dffc4a9a79bb2
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-04-18 13:54:51 -0300

    03) A ligação entre as ações do usuário e o modelo - 03) Evitando percorrer o DOM muitas vezes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,15 +1,16 @@
 class NegociacaoController{
 
+    constructor(){
+        //coloca a função dentro da variável com associação com o document
+       let $ = document.querySelector.bind(document);
+       this.inputQuantidade = $('#quantidade');
+       this.inputData = $('#data');
+       this.inputValor = $('#valor');
+    }
+
     adiciona(event){
         event.preventDefault();
-       // alert("Chamei ação no controller");
-
-       //coloca a função dentro da variável com associação com o document
-       let $ = document.querySelector.bind(document);
-       let inputQuantidade = $('#quantidade');
-       let inputData = $('#data');
-       let inputValor = $('#valor');
-
-       console.log(inputData.value);
+      
+      console.log(this.inputData.value);
     }
 }
