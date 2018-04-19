3479ba654ae6b8d064b59bcd77497c57d2097832
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-04-19 13:19:44 -0300

    03) A ligação entre as ações do usuário e o modelo - 08) Resolvendo um problema com o paradigma funcional

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,22 +1,37 @@
-class NegociacaoController{
+class NegociacaoController {
 
-    constructor(){
-        //coloca a função dentro da variável com associação com o document
-       let $ = document.querySelector.bind(document);
-       this._inputQuantidade = $('#quantidade');
-       this._inputData = $('#data');
-       this._inputValor = $('#valor');
+    constructor() {
+
+        let $ = document.querySelector.bind(document);//coloca a função dentro da variável com associação com o document
+        this._inputQuantidade = $('#quantidade');
+        this._inputData = $('#data');
+        this._inputValor = $('#valor');
     }
 
-    adiciona(event){
-        
+    adiciona(event) {
+
         event.preventDefault();
 
-        let data = new Date(this._inputData.value.replace(/-/g,','));
+        let data = new Date(
+            ...this._inputData.value
+                .split('-')
+
+                /*.map(function (item, indice) {
+                    if (indice == 1) {
+                        return item - 1;
+                    }
+                    else{
+                        return item;
+                    }
+
+                })*/
+        );
+
+        console.log(data);
 
         //let negociacao = new Negociacao(this._inputData.value, this._inputValor.value, this._inputQuantidade.value);
-        
-        
+
+
         console.log(data);
     }
 }
