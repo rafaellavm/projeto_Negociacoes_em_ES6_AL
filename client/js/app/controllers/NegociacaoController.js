c1fa3c9bbdb62f7311e6140de54c4893d3c8a50b
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-04-26 15:28:12 -0300

    04) Lidar com data é trabalhoso? Chame um ajudante! - 02) Isolando a responsabilidade de conversão de datas

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -12,24 +12,15 @@ class NegociacaoController {
 
         event.preventDefault();
 
-        let data = new Date(
-            ...this._inputData.value
-                .split('-')
-
-                .map((item, indice) => item - indice %2)
-        );
-
+        let helper = new DataHelper();
+        
         let negociacao = new Negociacao( 
-            data,
+            helper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value,
             );
-
-        let diaMesAno = negociacao.data.getDate() +
-        "/" + (negociacao.data.getMonth() + 1) +
-        "/" + negociacao.data.getFullYear();
-
-
-        console.log(diaMesAno);
+   
+        console.log(negociacao);
+        console.log(helper.dataParaTexto(negociacao.data));
     }
 }
