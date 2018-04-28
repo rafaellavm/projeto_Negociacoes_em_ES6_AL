a548970dcc58af64092d6d8b9762dd9ff8514a2d
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-04-28 15:19:29 -0300

    04) Lidar com data é trabalhoso? Chame um ajudante! - 05) Criando nosso ListModel

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,25 +2,40 @@ class NegociacaoController {
 
     constructor() {
 
-        let $ = document.querySelector.bind(document);//coloca a função dentro da variável com associação com o document
+        let $ = document.querySelector.bind(document); //coloca a função dentro da variável com associação com o document
         this._inputQuantidade = $('#quantidade');
         this._inputData = $('#data');
         this._inputValor = $('#valor');
+
+        this._listaNegociacoes = new ListaNegociacoes();
+        
     }
 
     adiciona(event) {
 
         event.preventDefault();
 
-        //let helper = new DataHelper();
-        
-        let negociacao = new Negociacao( 
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criaNegociacao(){
+
+        return new Negociacao(
             DataHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value,
-            );
-   
-        console.log(negociacao);
-        console.log(DataHelper.dataParaTexto(negociacao.data));
+        );
+
+    }
+    //underline pra ser só chamado pela classe controller, só pode ser chanado dentro dela
+    _limpaFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = '1';
+        this._inputValor.value = '0.0';
+        
+        this._inputData.focus();
     }
 }
