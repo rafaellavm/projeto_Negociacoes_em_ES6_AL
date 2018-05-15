91ee4597a6fd0fb4f0ce6c469032b2f2877aa098
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-05-15 18:06:09 -0300

    06) Generalizando a solução da nossa View - 02) Criando a classe MensagemView

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -8,10 +8,15 @@ class NegociacaoController {
         this._inputValor = $('#valor');
 
         this._listaNegociacoes = new ListaNegociacoes();
+ 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-
         this._negociacoesView.update(this._listaNegociacoes);
-        
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
+
+
     }
 
     adiciona(event) {
@@ -20,13 +25,17 @@ class NegociacaoController {
 
         //console.log(this._criaNegociacao());
         this._listaNegociacoes.adiciona(this._criaNegociacao());
+
+        this._mensagem.texto = 'Negociacao adicionada com sucesso';
+        this._mensagemView.update(this._mensagem);
+
         this._negociacoesView.update(this._listaNegociacoes);
 
         this._limpaFormulario();
-        
+
     }
 
-    _criaNegociacao(){
+    _criaNegociacao() {
 
         return new Negociacao(
             DataHelper.textoParaData(this._inputData.value),
@@ -36,11 +45,11 @@ class NegociacaoController {
 
     }
     //underline pra ser só chamado pela classe controller, só pode ser chanado dentro dela
-    _limpaFormulario(){
+    _limpaFormulario() {
         this._inputData.value = '';
         this._inputQuantidade.value = '1';
         this._inputValor.value = '0.0';
-        
+
         this._inputData.focus();
     }
 }
